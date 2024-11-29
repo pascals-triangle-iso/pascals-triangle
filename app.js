7464bfa5fba0cd7c99fbcf0e9dcec590051f7fc9
pascals-triangle-iso <pascals.triangle.iso@gmail.com>
2024-11-29 17:17:18 +0300

    Added navbar

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ function setLanguage(languageCode) {
             document.title = data.title || 'Pascal\'s Triangle';
             document.getElementById('selected-lang').textContent = data.language || 'Null';
             document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
+            document.getElementById('navbar-history').textContent = data.navbar_history || 'Null';
+            document.getElementById('navbar-modelling').textContent = data.navbar_modelling || 'Null';
+            console.log(data.language);
+            console.log(data.title);
+            console.log(data.navbar_history);
+            console.log(data.navbar_modelling);
         })
         .catch(error => {
             console.error('Error loading language file:', error);
