16ea0e4073bf26c191877c7bf796e3c28296a20e
pascals-triangle-iso <pascals.triangle.iso@gmail.com>
2024-11-30 18:06:00 +0300

    Test commit
    Contains unfixed bugs

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ function setLanguage(languageCode) {
             document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
             document.getElementById('navbar-history').textContent = data.navbar_history || 'Null';
             document.getElementById('navbar-modelling').textContent = data.navbar_modelling || 'Null';
+            document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
+            document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
             document.getElementById('history-paragraph').innerHTML = data.history_paragraph || 'Null';
             console.log(data.source);
         })
