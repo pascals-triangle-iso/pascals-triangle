78326effcf7840c55b81eeee8eb3b39c3f63a1fb
pascals-triangle-iso <pascals.triangle.iso@gmail.com>
2024-12-01 00:55:22 +0300

    Added footer + bugfix

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ function setLanguage(languageCode) {
     fetch('language/' + languageCode + '.json')
         .then(response => response.json())
         .then(data => {
+            const currentPage = window.location.pathname;
+
             document.title = data.title || 'Pascal\'s Triangle';
+
             document.getElementById('selected-lang').textContent = data.language || 'Null';
             document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
             document.getElementById('navbar-history').textContent = data.navbar_history || 'Null';
             document.getElementById('navbar-modelling').textContent = data.navbar_modelling || 'Null';
-            document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
-            document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
-            document.getElementById('history-paragraph').innerHTML = data.history_paragraph || 'Null';
-            console.log(data.source);
+
+            if(currentPage === '/index.html' || currentPage === '/') {
+                document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
+                document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
+            }
+            
+            if(currentPage === '/history.html') {
+                document.getElementById('history-paragraph').innerHTML = data.history_paragraph || 'Null';
+            }
         })
         .catch(error => {
             console.error('Error loading language file:', error);
