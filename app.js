6a1ecab4c2f4951278a29c6ad8fb3819ff1cd2d7
pascals-triangle-iso <pascals.triangle.iso@gmail.com>
2024-12-01 13:07:09 +0300

    Added quiz.html

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ function setLanguage(languageCode) {
             document.getElementById('selected-lang').style.setProperty('--flag-image', 'url(res/flags/' + languageCode + '.png)' || '#000');
             document.getElementById('navbar-history').textContent = data.navbar_history || 'Null';
             document.getElementById('navbar-modelling').textContent = data.navbar_modelling || 'Null';
+            document.getElementById('navbar-quiz').textContent = data.navbar_quiz || 'Null';
 
             if(currentPage === '/index.html' || currentPage === '/') {
                 document.querySelector('.introduction .text').innerHTML = data.introduction_text || 'Null';
                 document.querySelector('.text-container.history .text').innerHTML = data.history_text || 'Null';
+                document.querySelector('.text-container.modelling .text').innerHTML = data.modelling_text || 'Null';
+                document.querySelector('.text-container.quiz .text').innerHTML = data.quiz_text || 'Null';
             }
             
             if(currentPage === '/history.html') {
